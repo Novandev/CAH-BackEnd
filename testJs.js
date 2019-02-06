74a2b0bcff27cb0b50d207fc40c83703881492c6
Novandev <donovan.adams@students.makeschool.com>
2019-02-06 07:04:02 -0800

    added secd and third expansion packs to the models

--- testJs.js
+++ testJs.js
@@ -7,10 +7,18 @@ mongoose.connect('mongodb://localhost:27017/CAH', {useNewUrlParser: true});
 var cards = require('./fullCardsList')
 // const allBlackCardsModel = mongoose.model('AllBlackCards', { text: String, pick: Number });
 // const allWhiteCardsModel = mongoose.model('AllWhiteCards', { text: String});
-const baseSetBlackModel = mongoose.model('BaseSetBlackCards', { text: String, pick: Number });
-const baseSetWhiteModel = mongoose.model('BaseSetWhiteCards', { text: String});
+// const baseSetBlackModel = mongoose.model('BaseSetBlackCards', { text: String, pick: Number });
+// const baseSetWhiteModel = mongoose.model('BaseSetWhiteCards', { text: String});
 
-// console.log(cards.allCards['order'])
+// const firstExpansionBlackModel = mongoose.model('FirstExpansionBlackCards', { text: String, pick: Number });
+// const firstExpansionWhiteModel = mongoose.model('FirstExpansionWhiteCards', { text: String});
+
+// const secondExpansionBlackModel = mongoose.model('secondExpansionBlackCards', { text: String, pick: Number });
+// const secondExpansionWhiteModel = mongoose.model('secondExpansionWhiteCards', { text: String});
+
+// const thirdExpansionBlackModel = mongoose.model('thirdExpansionBlackCards', { text: String, pick: Number });
+// const thirdExpansionWhiteModel = mongoose.model('thirdExpansionWhiteCards', { text: String});
+
 // All cards from file
 const allBlackcards = cards.allCards['blackCards']
 
@@ -22,7 +30,23 @@ const allWhiteCards = cards.allCards['whiteCards']
 // const baseSetWhiteCards = allWhiteCards.slice(0,460)
 
 
-// Slices for
+// Slices for The First Expansion
+// const firstExpansionBlack = allBlackcards.slice(90,110)
+
+// const firstExpansionWhite = allWhiteCards.slice(460,540)
+
+// Slices for The Second Expansion
+// const secondExpansionBlack = allBlackcards.slice(110,135)
+
+// const secondExpansionWhite = allWhiteCards.slice(540,615)
+
+// Slices for The Second Expansion
+// const thirdExpansionBlack = allBlackcards.slice(135,160)
+
+// const thirdExpansionWhite = allWhiteCards.slice(615,690)
+
+
+
 
 // creation of all black cards from json file
 // for( let card of allBlackcards){
@@ -67,3 +91,72 @@ const allWhiteCards = cards.allCards['whiteCards']
 //         console.log(err)
 //     })
 // }
+
+
+// // creation of First expansion black model
+// for( let card of firstExpansionBlack){
+//     let newCard = new firstExpansionBlackModel(card)
+//     newCard.save().then(card =>{
+//         console.log(card)
+//     }).catch(err =>{
+//         console.log(err)
+//     })
+// }
+//
+// // creation of First expansion white cards model
+//
+// for( let card of firstExpansionWhite){
+//     let newCard = new firstExpansionWhiteModel({text: card})
+//     console.log(newCard)
+//     newCard.save().then(card =>{
+//         console.log(card)
+//     }).catch(err =>{
+//         console.log(err)
+//     })
+// }
+
+// // creation of second expansion black model
+// for( let card of secondExpansionBlack){
+//     let newCard = new secondExpansionBlackModel(card)
+//     newCard.save().then(card =>{
+//         console.log(card)
+//     }).catch(err =>{
+//         console.log(err)
+//     })
+// }
+//
+// // creation of second expansion white cards model
+//
+// for( let card of secondExpansionWhite){
+//     let newCard = new secondExpansionWhiteModel({text: card})
+//     console.log(newCard)
+//     newCard.save().then(card =>{
+//         console.log(card)
+//     }).catch(err =>{
+//         console.log(err)
+//     })
+// }
+
+
+// // creation of third expansion black model
+// for( let card of thirdExpansionBlack){
+//     let newCard = new thirdExpansionBlackModel(card)
+//     newCard.save().then(card =>{
+//         console.log(card)
+//     }).catch(err =>{
+//         console.log(err)
+//     })
+// }
+//
+// // creation of third expansion white cards model
+//
+// for( let card of thirdExpansionWhite){
+//     let newCard = new thirdExpansionWhiteModel({text: card})
+//     console.log(newCard)
+//     newCard.save().then(card =>{
+//         console.log(card)
+//     }).catch(err =>{
+//         console.log(err)
+//     })
+// }
+
