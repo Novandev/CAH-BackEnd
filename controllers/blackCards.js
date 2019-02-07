c48ebfe17903d971ff07a99838c6cc88aa6ee3b5
Novandev <donovan.adams@students.makeschool.com>
2019-02-07 13:30:35 -0800

    addedalmost all balckcards routes

--- controllers/blackCards.js
+++ controllers/blackCards.js
@@ -54,6 +54,183 @@ router.get('/baseset',(req,res)=>{
     })
 })
 
+router.get('/firstExpansion',(req,res)=>{
+    firstExpansionBlack.find({}).then(cards =>{
+        res.status(200).json(cards)
+    }).catch(err =>{
+        res.status(400).send(err.message)
+    })
+})
 
+
+router.get('/secondExpansion',(req,res)=>{
+    secondExpansionBlack.find({}).then(cards =>{
+        res.status(200).json(cards)
+    }).catch(err =>{
+        res.status(400).send(err.message)
+    })
+})
+
+router.get('/thirdExpansion',(req,res)=>{
+    thirdExpansionBlack.find({}).then(cards =>{
+        res.status(200).json(cards)
+    }).catch(err =>{
+        res.status(400).send(err.message)
+    })
+})
+
+router.get('/fourthExpansion',(req,res)=>{
+    fourthExpansionBlack.find({}).then(cards =>{
+        res.status(200).json(cards)
+    }).catch(err =>{
+        res.status(400).send(err.message)
+    })
+})
+
+router.get('/fifthExpansion',(req,res)=>{
+    fifthExpansionBlack.find({}).then(cards =>{
+        res.status(200).json(cards)
+    }).catch(err =>{
+        res.status(400).send(err.message)
+    })
+})
+
+router.get('/sixthExpansion',(req,res)=>{
+    sixthExpansionBlack.find({}).then(cards =>{
+        res.status(200).json(cards)
+    }).catch(err =>{
+        res.status(400).send(err.message)
+    })
+})
+
+router.get('/greenboxExpansion',(req,res)=>{
+    greenboxExpansionBlack.find({}).then(cards =>{
+        res.status(200).json(cards)
+    }).catch(err =>{
+        res.status(400).send(err.message)
+    })
+})
+
+router.get('/ninetiesNostalgia',(req,res)=>{
+    ninetiesNostalgiaBlack.find({}).then(cards =>{
+        res.status(200).json(cards)
+    }).catch(err =>{
+        res.status(400).send(err.message)
+    })
+})
+
+router.get('/fantasyPack',(req,res)=>{
+    fantasyPackBlack.find({}).then(cards =>{
+        res.status(200).json(cards)
+    }).catch(err =>{
+        res.status(400).send(err.message)
+    })
+})
+
+router.get('/foodPack',(req,res)=>{
+    foodPackBlack.find({}).then(cards =>{
+        res.status(200).json(cards)
+    }).catch(err =>{
+        res.status(400).send(err.message)
+    })
+})
+
+router.get('/sciencePack',(req,res)=>{
+    sciencePackBlack.find({}).then(cards =>{
+        res.status(200).json(cards)
+    }).catch(err =>{
+        res.status(400).send(err.message)
+    })
+})
+
+router.get('/worldWideWebPack',(req,res)=>{
+    worldWideWebPackBlack.find({}).then(cards =>{
+        res.status(200).json(cards)
+    }).catch(err =>{
+        res.status(400).send(err.message)
+    })
+})
+
+router.get('/voteForHillaryPack',(req,res)=>{
+    voteForHillaryPackBlack.find({}).then(cards =>{
+        res.status(200).json(cards)
+    }).catch(err =>{
+        res.status(400).send(err.message)
+    })
+})
+
+router.get('/voteForTrumpPack',(req,res)=>{
+    voteForTrumpPackBlack.find({}).then(cards =>{
+        res.status(200).json(cards)
+    }).catch(err =>{
+        res.status(400).send(err.message)
+    })
+})
+
+router.get('/trumpSurvivalPack',(req,res)=>{
+    trumpSurvivalPackBlack.find({}).then(cards =>{
+        res.status(200).json(cards)
+    }).catch(err =>{
+        res.status(400).send(err.message)
+    })
+})
+
+
+router.get('/holidayPack2012',(req,res)=>{
+    holidayPack2012Black.find({}).then(cards =>{
+        res.status(200).json(cards)
+    }).catch(err =>{
+        res.status(400).send(err.message)
+    })
+})
+
+router.get('/holidayPack2013',(req,res)=>{
+    holidayPack2013Black.find({}).then(cards =>{
+        res.status(200).json(cards)
+    }).catch(err =>{
+        res.status(400).send(err.message)
+    })
+})
+
+
+router.get('/paxEast2013',(req,res)=>{
+    paxEast2013Black.find({}).then(cards =>{
+        res.status(200).json(cards)
+    }).catch(err =>{
+        res.status(400).send(err.message)
+    })
+})
+
+router.get('/paxEast2014',(req,res)=>{
+    paxEast2014Black.find({}).then(cards =>{
+        res.status(200).json(cards)
+    }).catch(err =>{
+        res.status(400).send(err.message)
+    })
+})
+
+router.get('/paxPrime2013',(req,res)=>{
+    paxPrime2013Black.find({}).then(cards =>{
+        res.status(200).json(cards)
+    }).catch(err =>{
+        res.status(400).send(err.message)
+    })
+})
+
+router.get('/paxEast2014PanelPack',(req,res)=>{
+    paxEast2014PanelPackBlack.find({}).then(cards =>{
+        res.status(200).json(cards)
+    }).catch(err =>{
+        res.status(400).send(err.message)
+    })
+})
+
+router.get('/paxPrime2014PanelPack',(req,res)=>{
+    paxPrime2014PanelPackBlack.find({}).then(cards =>{
+        res.status(200).json(cards)
+    }).catch(err =>{
+        res.status(400).send(err.message)
+    })
+})
 
 module.exports = router;
